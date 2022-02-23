3cb3d5148bf47f2c5062d61642de731dafd7d592
dvlpr1996 <nimajahanbakhshian@gmail.com>
2022-02-24 02:36:12 +0330

    edit tailwind.config.js file and add xs breakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    screens: {
+      'xs': '360px',
+
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: []
 }

ddf633d3e3456ce4f3fee303a2b7c4e0b306a99c
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-04-30 14:54:37 +0330

    fix font issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,17 @@
 module.exports = {
-  content: ["./*.html"],
+  content: ["./**/*.html"],
   theme: {
     screens: {
       'xs': '360px',
-
       'sm': '640px',
-
       'md': '768px',
-
       'lg': '1024px',
-
       'xl': '1280px',
-
       '2xl': '1536px',
     },
     extend: {},
   },
-  plugins: []
+  plugins: [
+    require('tailwindcss-debug-screens')
+  ]
 }

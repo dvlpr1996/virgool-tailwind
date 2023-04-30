efeef0db7e8b18b3233c2180e7ca6bfc2bff1fd0
dvlpr1996 <nimajahanbakhshian@gmail.com>
2023-04-30 19:31:02 +0330

    add alpinejs cdn and refactor html pages with alpinejs

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,14 @@
-$(document).ready((function(){$("#search-bar").hide(),$("#search-btn").click((function(){$("#search-bar").show(),$("#close-btn").click((function(){$("#search-bar").hide()}))})),$("#sidebar").hide(),$(document).scroll((function(){let y=$(this).scrollTop();y>150&&y<1600?$("#sidebar").show():$("#sidebar").hide()}))}));
+$(document).ready(function () {
+  $("#search-bar").hide(),
+    $("#search-btn").click(function () {
+      $("#search-bar").show(),
+        $("#close-btn").click(function () {
+          $("#search-bar").hide();
+        });
+    }),
+    $("#sidebar").hide(),
+    $(document).scroll(function () {
+      let y = $(this).scrollTop();
+      y > 150 && y < 1600 ? $("#sidebar").show() : $("#sidebar").hide();
+    });
+});
